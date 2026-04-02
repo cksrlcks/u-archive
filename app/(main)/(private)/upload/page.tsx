6b7975381e22b5e285aabb350c2cb73789a33044
cksrlcks <chanki.kim89@gmail.com>
2026-04-03 08:19:26 +0900

    feat: 업로드 페이지 이너 수정

--- app/(main)/(private)/upload/page.tsx
+++ app/(main)/(private)/upload/page.tsx
@@ -4,6 +4,7 @@ import PageHeader from "@/components/common/page-header"
 import NotApproved from "@/features/auth/components/not-approved"
 import { auth } from "@/features/auth/lib/auth"
 import { UploadForm } from "@/features/work/components/upload-form"
+import NarrowInner from "@/components/layout/narrow-inner"
 
 export default async function Page() {
   const session = await auth.api.getSession({ headers: await headers() })
@@ -13,9 +14,9 @@ export default async function Page() {
   }
 
   return (
-    <>
+    <NarrowInner>
       <PageHeader title="업로드" />
       <UploadForm />
-    </>
+    </NarrowInner>
   )
 }
